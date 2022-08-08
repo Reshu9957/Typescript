3c3811b2d281b04426109fe283d19f31bbdea17a
Reshu9957 <87746051+Reshu9957@users.noreply.github.com>
2022-08-08 10:05:04 +0530

    Add files via upload

---
+++ Main/main.js
@@ -0,0 +1,32 @@
+"use strict";
+exports.__esModule = true;
+var message = 'Welcome back';
+console.log(message);
+var x = 10;
+var y = 20;
+var title = 'codevolution';
+//variable types boolean,number,string
+var isBeginner = true;
+var total = 0;
+var Name = 'Rose';
+var sentence = "my name is ".concat(Name, " I am a biginner in Typescript");
+console.log(sentence);
+var n = null;
+var u = undefined;
+var isNew = true;
+var myName = 'Carly';
+var list1 = [1, 2, 3];
+var list2 = [1, 2, 3];
+var person = ['chris', 22];
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+;
+var c = Color.Blue;
+console.log(c);
+var randomvalue = 10;
+randomvalue = true;
+randomvalue = 'vishwas';
---
+++ Main/main.ts
@@ -0,0 +1,53 @@
+export{}
+let message = 'Welcome back' ;
+console.log (message) 
+ 
+let x = 10;
+const y = 20;
+
+const title = 'codevolution';
+
+//variable types boolean,number,string
+
+let isBeginner:boolean = true ;
+let total:number = 0;
+const Name = 'Rose';
+
+
+let sentence  = `my name is ${Name} I am a biginner in Typescript`;
+
+console.log(sentence)
+
+let n : null = null; 
+let u: undefined = undefined;
+
+let isNew : boolean = true;
+let myName : string = 'Carly' ;
+
+let list1 : number[] = [1,2,3] ;
+let list2 : Array<number> = [1,2,3] ;
+
+let person : [string , number] = ['chris' , 22];
+
+enum Color {Red , Green , Blue};
+
+let c : Color = Color.Blue;
+
+console.log (c);
+
+let randomvalue : any = 10;
+randomvalue = true;
+randomvalue = 'vishwas' ;
+
+
+
+
+
+
+
+
+
+
+
+
+
